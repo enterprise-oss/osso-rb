ea61797dbf4636323069c4397490f38138837353
Sam Bauch <sbauch@gmail.com>
2020-08-13 14:48:45 -0400

    gotta stop this

--- lib/osso/graphql/mutation.rb
+++ lib/osso/graphql/mutation.rb
@@ -6,14 +6,12 @@ module Osso
   module GraphQL
     module Types
       class MutationType < BaseObject
-        field :add_redirect_uris_to_oauth_client, mutation: Mutations::AddRedirectUrisToOauthClient, null: false
         field :configure_identity_provider, mutation: Mutations::ConfigureIdentityProvider, null: true
         field :create_identity_provider, mutation: Mutations::CreateIdentityProvider
         field :create_enterprise_account, mutation: Mutations::CreateEnterpriseAccount
         field :create_oauth_client, mutation: Mutations::CreateOauthClient
         field :delete_enterprise_account, mutation: Mutations::DeleteEnterpriseAccount
         field :delete_oauth_client, mutation: Mutations::DeleteOauthClient
-        field :delete_redirect_uri, mutation: Mutations::DeleteRedirectUri
         field :set_redirect_uris, mutation: Mutations::SetRedirectUris
         field :regenerate_oauth_credentials, mutation: Mutations::RegenerateOauthCredentials
       end
