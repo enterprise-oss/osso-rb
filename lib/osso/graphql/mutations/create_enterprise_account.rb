54cdb40efac82d1cfa9f93e0efeeb934187eb31c
Sam Bauch <sbauch@gmail.com>
2020-08-25 14:03:04 -0400

    fix customer creation

--- lib/osso/graphql/mutations/create_enterprise_account.rb
+++ lib/osso/graphql/mutations/create_enterprise_account.rb
@@ -8,18 +8,23 @@ module Osso
 
         argument :domain, String, required: true
         argument :name, String, required: true
-        argument :oauth_client_id, ID, required: false
+        argument :oauth_client_id, String, required: false
 
         field :enterprise_account, Types::EnterpriseAccount, null: false
         field :errors, [String], null: false
 
         def resolve(**args)
           enterprise_account = Osso::Models::EnterpriseAccount.new(args)
-          enterprise_account.oauth_client_id ||= context[:oauth_client_id]
+          enterprise_account.oauth_client_id ||= find_client_db_id(context[:oauth_client_id])
 
           return response_data(enterprise_account: enterprise_account) if enterprise_account.save
 
           response_error(errors: enterprise_account.errors.full_messages)
+        end
+
+        def find_client_db_id(oauth_client_identifier)
+          Osso::Models::OauthClient.find_by(identifier: oauth_client_identifier).
+            id
         end
       end
     end
