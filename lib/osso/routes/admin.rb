572cae91277a26c7fd020fcb634a9a97488a9975
Sam Bauch <sbauch@gmail.com>
2020-09-17 21:23:39 -0400

    Remove layout for admin (#26)

--- lib/osso/routes/admin.rb
+++ lib/osso/routes/admin.rb
@@ -16,37 +16,37 @@ module Osso
       get '/login' do
         token_protected!
 
-        erb :admin
+        erb :admin, layout: false
       end
 
       get '' do
         internal_protected!
 
-        erb :admin
+        erb :admin, layout: false
       end
 
       get '/enterprise' do
         token_protected!
 
-        erb :admin
+        erb :admin, layout: false
       end
 
       get '/enterprise/:domain' do
         enterprise_protected!(params[:domain])
 
-        erb :admin
+        erb :admin, layout: false
       end
 
       get '/config' do
         admin_protected!
 
-        erb :admin
+        erb :admin, layout: false
       end
 
       get '/config/:id' do
         admin_protected!
 
-        erb :admin
+        erb :admin, layout: false
       end
     end
   end
