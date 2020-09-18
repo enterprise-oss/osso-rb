ac1274489af0921f2ba4519377500b87c3a1bf53
Sam Bauch <sbauch@gmail.com>
2020-09-18 09:26:10 -0400

    refactored, green

--- lib/osso.rb
+++ lib/osso.rb
@@ -5,6 +5,7 @@ module Osso
   require_relative 'osso/lib/app_config'
   require_relative 'osso/lib/oauth2_token'
   require_relative 'osso/lib/route_map'
+  require_relative 'osso/lib/saml_handler'
   require_relative 'osso/models/models'
   require_relative 'osso/routes/routes'
   require_relative 'osso/graphql/schema'
